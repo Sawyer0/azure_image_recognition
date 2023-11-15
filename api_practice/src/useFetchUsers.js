3622a88111e9c9f5bbb9d3912dcd58fb432b18b8
Dawan Sawyer <dawansawyer@gmail.com>
2023-11-14 22:23:56 -0500

    created the custom hook for fetching users and their data. now this data can be used anywhere the custom hook is implemented

--- api_practice/src/useFetchUsers.js
+++ api_practice/src/useFetchUsers.js
@@ -1,15 +1,27 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const useFetchUsers = (url) => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(null);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-    useEffect(() => {
-        setLoading(true);
-        axios.get
-    })
+  useEffect(() => {
+    setLoading(true);
+    axios.get(url)
+      .then(response => {
+        setData(response.data);
+        setError(null);
+      })
+      .catch(error => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
 
-
+  return { data, error, loading };
 };
+
+export default useFetchUsers;
